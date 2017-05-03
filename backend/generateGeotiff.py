5590582fba18f94bec0fab4bd82982ce7d91de3b
Harri Pitkänen <harri.pitkanen@luke.fi>
2017-05-03 12:42:07 +0300

    Save destination dataset (still identical to source dataset)

--- backend/generateGeotiff.py
+++ backend/generateGeotiff.py
@@ -1,8 +1,14 @@
+from osgeo import gdal, ogr
 
-# TODO almost everything
+src_filename = "../../aineisto/Clc2012_FI20m_Espoo.tif"
+dst_filename = "../../output/mustikka.tif"
 
-src_ds = gdal.Open( src_filename )
-dst_ds = driver.CreateCopy( dst_filename, src_ds, 0 )
+src_ds = gdal.Open(src_filename)
+driver = src_ds.GetDriver()
+dst_ds = driver.CreateCopy(dst_filename, src_ds, 0)
+
+# TODO modify the destination dataset
+
 # Once we're done, close properly the dataset
 dst_ds = None
 src_ds = None
